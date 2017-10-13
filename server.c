02e9a831d21c299ead431794c1d45cc6162a57cd
ErickRyu <ErickRyu>
2017-10-13 21:46:31 +0900

    Comment

--- server.c
+++ server.c
@@ -5,13 +5,10 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <stdint.h>
 #include <fcntl.h>
 #include <signal.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_t pthread;
 void error(char *msg)
 {
     perror(msg);
@@ -20,17 +17,18 @@ void error(char *msg)
 void cleanExit(){exit(0);}
 
 
-void *pthread_read_and_write(void *arg);
+char* getContentType(char *filename);
 int writeToClient(int newsockfd, char* msg);
 void sendError(int newsockfd);
-void sendResponseHeader(int newsockfd, char *httpMsg, long contentLen, char *contentType);
+int sendResponseHeader(int newsockfd, char *httpMsg, long contentLen, char *contentType);
 void requestHandler(int newsockfd, char *reqMsg);
 int main(int argc, char *argv[])
 {
-    //signal(SIGPIPE, SIG_IGN);
+    // Ignore SIGPIPE
     sigignore(SIGPIPE);
     signal(SIGTERM, cleanExit);
     signal(SIGINT, cleanExit);
+
     int sockfd, newsockfd; 
     int portno;
     socklen_t clilen;
@@ -78,84 +76,56 @@ int main(int argc, char *argv[])
         }
         requestHandler(newsockfd, reqMsg);
         bzero(reqMsg, 500);
-        printf("=============================\n");
-
-
-
-        /*
-        pthread_create(&pthread, NULL, *pthread_read_and_write, (void *)(intptr_t)newsockfd);
-        pthread_detach(pthread);
-        */
     }
     close(sockfd);
 
     return 0; 
 }
-
-void *pthread_read_and_write(void *arg){
-    int newsockfd = (int)arg;
-    char reqMsg[500];
-    ssize_t n;
-
-    bzero(reqMsg,500);
-    n = read(newsockfd,reqMsg,499); 
-    if(n == 0) return NULL;
-    if (n < 0) error("ERROR reading from socket");
-        printf("========Request Message======\n%s\n",reqMsg);
-        if(reqMsg[0] == 0){
-            printf("req msg is null\n");
-            close(newsockfd);
-            return NULL;
-        }
-        requestHandler(newsockfd, reqMsg);
-        bzero(reqMsg, 500);
-        printf("=============================\n");
-
-    return NULL;
+char *getContentType(char *fileName){
+    char tmpFileName[100];
+    strcpy(tmpFileName, fileName);
+    strtok(tmpFileName, ".");
+    char *extension = strtok(NULL, ".");
+    if(extension == NULL || strcmp(extension, "html") == 0){
+        return "text/html";
+    }else if(strcmp(extension, "jpeg") == 0){
+        return "image/jpeg";
+    }else if(strcmp(extension, "gif") == 0){
+        return "image/gif";
+    }else if(strcmp(extension, "mp3") == 0){
+        return "audio/mpeg";
+    }else if(strcmp(extension, "pdf") == 0){
+        return "application/pdf";
+    }
+    return "text/plain";
 }
+
 void requestHandler(int newsockfd, char *reqMsg){
 
-
-    printf("client socket : %d\n", newsockfd);
-
+    // Tokenizing http method and file name from request message
     char file[100];
     char *method = strtok(reqMsg, " /");
     strcpy(file, strtok(NULL, " /"));
-    char tmpFileName[100];
-    strcpy(tmpFileName, file);
-    strtok(tmpFileName, ".");
-    char *extension = strtok(NULL, ".");
-    printf("method : %s\n", method);
-    printf("file : %s\n", file);
-    printf("extension : %s\n", extension);
-    if(strcmp(method, "GET") == 0 || strcmp(method, "get") == 0){
 
-    }else{
+    // Return if http method is not GET
+    if(strcmp(method, "GET") != 0 && strcmp(method, "get") != 0){
         sendError(newsockfd);
         return;
     }
+
+    /* Use index.html as default file. But I commented this lines because I don't know what is your default index file.
     if(strcmp(file, "HTTP") == 0 || strcmp(file, "http") == 0){
         strcpy(file , "index.html");
     }
-    printf("compare success\n");
-    long fsize;
-    char type[20];
+    */
 
-    if(extension == NULL || strcmp(extension, "html") == 0){
-        strcpy(type, "text/html");
-    }else if(strcmp(extension, "jpeg") == 0){
-        strcpy(type,"image/jpeg");
-    }else if(strcmp(extension, "gif") == 0){
-        strcpy(type,"image/gif");
-    }else if(strcmp(extension, "mp3") == 0){
-        strcpy(type, "audio/mpeg");
-    }else if(strcmp(extension, "pdf") == 0){
-        strcpy(type, "application/pdf");
-    }else{
-        strcpy(type, "text/plain");
-    }
-    printf("compare success\n");
+    
+    // Get content-type by compare string of file's extention
+    char *type = getContentType(file);
     printf("type : %s\n", type);
+
+    // Calculate file size and Open file. When file is not exist, return 400 Bad Request
+    long fsize;
     FILE *fp = fopen(file, "rb");
     if(fp == NULL){
         sendError(newsockfd);
@@ -163,48 +133,21 @@ void requestHandler(int newsockfd, char *reqMsg){
     }
     fseek(fp, 0, SEEK_END);
     fsize = ftell(fp);
+    
+    // Read file to msg
     char *msg = (char*)malloc(fsize);
     rewind(fp);
     fread(msg, fsize, 1, fp);
     fclose(fp);
 
-    char rcvBuf[BUFSIZ+1];
-    int fd;
-    printf("reading file...\n");
-    if((fd = open(file, O_RDONLY)) <0 ){
-        printf("sending error...\n");
-        sendError(newsockfd);
-        printf("send error OK\n");
-        return;
-    }
-    printf("open fd OK\n");
-
+    // Send response message. If return is less than 0, client socket connection is closed. because write functino returned 0
     char *httpMsgOK = "200 OK";
-    printf("sending header...\n");
-    pthread_mutex_lock(&mutex);
-    sendResponseHeader(newsockfd, httpMsgOK, fsize, type);
-    printf("send header OK\n");
-
-    pthread_mutex_unlock(&mutex);
-    int n;
-    bzero(rcvBuf, BUFSIZ + 1);
-    /*
-    if(fd >= 0) {
-        while((n=read(fd, rcvBuf, BUFSIZ)) > 0){
-
-            printf("sending rcvBuf : %d, remain : %ld\n", n, fsize-=n);
-            ssize_t res = send(newsockfd, rcvBuf, n, 0);
-            if(n == EOF | n == 0) { printf("finish file\n"); break; }
-            if(res == 0) { printf("sending is 0\n");}
-            if(res < 0) {
-                char errMsg[100];
-                sprintf(errMsg, "ERROR writing to socket __sock : %d __", newsockfd);
-                error(errMsg);
-            }
-            bzero(rcvBuf, BUFSIZ + 1);
-        }
+    if( sendResponseHeader(newsockfd, httpMsgOK, fsize, type) < 0){
+        printf("Client socket connection closed\n");
+        close(newsockfd);
     }
-    */
+
+    // Write file to client
     ssize_t res = write(newsockfd, msg, fsize);
     if(res == 0){
         printf("end file\n");
@@ -212,8 +155,6 @@ void requestHandler(int newsockfd, char *reqMsg){
         error("ERROR writing to socket");
     }
     close(newsockfd);
-    printf("closed client socket\n");
-
 }
 void sendError(int newsockfd){
     char *msg = "<html><body><h1>400 Bad Request</h1></body></html>";
@@ -223,7 +164,7 @@ void sendError(int newsockfd){
     printf("closed client socket\n");
 }
 
-void sendResponseHeader(int newsockfd, char *httpMsg, long contentLen, char *contentType){
+int sendResponseHeader(int newsockfd, char *httpMsg, long contentLen, char *contentType){
     char resMsg[40];
     char conLen[100];
     char conType[50];
@@ -236,15 +177,7 @@ void sendResponseHeader(int newsockfd, char *httpMsg, long contentLen, char *con
     strcat(responseHeader, conType);
 
     printf("response message\n%s\n\n", responseHeader);
-    /*
-    printf("response message\n%s\n%s\n%s\n", resMsg, conLen, conType);
-    writeToClient(newsockfd, resMsg);
-    printf("send resMsg OK\n");
-    writeToClient(newsockfd, conLen);
-    writeToClient(newsockfd, conType);
-    */
-    writeToClient(newsockfd, responseHeader);
-    printf("send conType OK\n");
+    return writeToClient(newsockfd, responseHeader);
 }
 
 int writeToClient(int newsockfd, char* msg){
